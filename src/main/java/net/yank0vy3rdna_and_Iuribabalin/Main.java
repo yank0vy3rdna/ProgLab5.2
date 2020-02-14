f7a88bd6f4236f291767c1a1c66222ba43c87d48
iiuri <babalin2011@gmail.com>
2020-02-14 21:49:30 +0300

    Commands

--- src/main/java/net/yank0vy3rdna_and_Iuribabalin/Main.java
+++ src/main/java/net/yank0vy3rdna_and_Iuribabalin/Main.java
@@ -12,7 +12,6 @@ public class Main
     public static void main( String[] args )
     {
 
-
         HashMap<String, Executable> commands = new HashMap<>();
 
         commands.put("help", new HelpCommand());
