65b705af1a958d4cac9be18096826dcaa4d24f09
iiuri <babalin2011@gmail.com>
2020-02-14 23:33:26 +0300

    Commands

---
+++ src/main/java/net/yank0vy3rdna_and_Iuribabalin/App/ObjectInterfaces/Storable.java
@@ -0,0 +1,5 @@
+package net.yank0vy3rdna_and_Iuribabalin.App.ObjectInterfaces;
+
+public interface Storable {
+    void insert(StoredType hum);
+}
--- src/main/java/net/yank0vy3rdna_and_Iuribabalin/Commands/AddCommand.java
+++ src/main/java/net/yank0vy3rdna_and_Iuribabalin/Commands/AddCommand.java
@@ -1,15 +1,18 @@
 package net.yank0vy3rdna_and_Iuribabalin.Commands;
 
 import net.yank0vy3rdna_and_Iuribabalin.App.Dispatcher;
+import net.yank0vy3rdna_and_Iuribabalin.App.ObjectInterfaces.Storable;
+import net.yank0vy3rdna_and_Iuribabalin.App.ObjectInterfaces.StoredTypeReader;
 import net.yank0vy3rdna_and_Iuribabalin.App.UI;
 import net.yank0vy3rdna_and_Iuribabalin.Dragon.DragonReader;
 
 public class AddCommand implements Executable{
-    DragonReader reader = new DragonReader();
+    private StoredTypeReader reader;
+    private Storable storable;
     @Override
     @SuppressWarnings("unchecked")
     public String exec(String command, Dispatcher dispatcher) {
-        reader.setUI(new UI());
+        reader.setUi(new UI());
         storable.insert(reader.create());
         return "Добавлено";
     }
