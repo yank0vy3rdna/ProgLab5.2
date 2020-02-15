459d2bdbc712fba91fd06645d4c3c3b0e6b27282
iiuri <babalin2011@gmail.com>
2020-02-16 02:34:42 +0300

    Commands

--- src/main/java/net/yank0vy3rdna_and_Iuribabalin/Commands/SumOfAgesCommand.java
+++ src/main/java/net/yank0vy3rdna_and_Iuribabalin/Commands/SumOfAgesCommand.java
@@ -1,10 +1,7 @@
 package net.yank0vy3rdna_and_Iuribabalin.Commands;
 
-import net.yank0vy3rdna_and_Iuribabalin.App.CollectionWorker;
 import net.yank0vy3rdna_and_Iuribabalin.App.Dispatcher;
 import net.yank0vy3rdna_and_Iuribabalin.App.ObjectInterfaces.StoredType;
-import net.yank0vy3rdna_and_Iuribabalin.App.ObjectInterfaces.StoredTypeReader;
-import net.yank0vy3rdna_and_Iuribabalin.JSON.Workerable;
 
 public class SumOfAgesCommand implements Executable {
     @Override
